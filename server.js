0ea57f0c4c3dfdfc1c95549893cedb1a7aad10d9
martins chukwuemeka <donmartinspm@gmail.com>
2018-07-06 12:02:11 +0100

    Add project page

--- server.js
+++ server.js
@@ -12,8 +12,8 @@ app.set('view engine', 'hbs');
 app.use((req, res, next) => { 
     var now = new Date().toString();
     var log = `${now}: ${req.method} ${req.url}`;
-
     console.log(log);
+    
     fs.appendFile('server.log', log + '\n', (err) => {
         if (err) {
             console.log('Unable to append to server.log.')
@@ -29,7 +29,7 @@ app.use((req, res, next) => {
 app.use(express.static(__dirname + '/public' ));
 
 hbs.registerHelper('getCurrentYear', () => {
-     return new Date().getFullYear()
+     return new Date().getFullYear();
 });
 
 hbs.registerHelper('screamIt', (text) => {
